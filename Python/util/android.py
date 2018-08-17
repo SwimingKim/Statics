a22b166bea611e767d3bfeca04d6ea62a95bbf7b
skim <rumex13@gmail.com>
2018-08-17 21:55:58 +0900

    image reload

--- Python/util/android.py
+++ Python/util/android.py
@@ -1,130 +1,95 @@
 import subprocess
 import os
 from tkinter import *
-from PIL import Image
-from PIL import ImageTk
-
-scale = 0.5
+import os
 
 def sendMessage(deviceId, message, result=False):
-    cmd = "%s -s %s %s" % (adbPath(), deviceId, message)
     if result :
-        text = subprocess.check_output(cmd , shell=True)
+        text = subprocess.check_output("adb -s %s %s" % (deviceId, message) , shell=True)
         return text
     else :
-        os.system(cmd)
+        os.system("adb -s %s %s" % (deviceId, message))
         return "no text"
 
 def screenshot() :
-    sendMessage(conectedDevices[0], "shell screencap -p /sdcard/screen.jpg")
-    sendMessage(conectedDevices[0], "pull /sdcard/screen.jpg")
+    sendMessage(conectedDevices[0], "shell screencap -p /sdcard/screen.png")
+    sendMessage(conectedDevices[0], "pull /sdcard/screen.png")
 
-def adbPath() :
-    return "/Users/suyoung/Library/Android/sdk/platform-tools/adb"
-
-cmd = '{} devices'.format(adbPath())
-devices = subprocess.check_output(cmd, shell=True)
+devices = subprocess.check_output('adb devices', shell=True)
 conectedDevices = list()
 
-devicelist = str(devices).split('\\n')
+devicelist = str(devices).split('\\r\\n')
+del devicelist[0]
 for device in devicelist:
-    if len(device) != 28:
-        continue
+    device = device.replace(" ", "")
+    device = device.replace("'", "")
     device = device.replace("\\tdevice", "")
-    conectedDevices.append(device)
+    if len(device) >= 16:
+        conectedDevices.append(device)
 
+print(conectedDevices)
 wmSize = sendMessage(conectedDevices[0], "shell wm size", True)
 wmSize = list(str(wmSize).split("\\nOverride size: "))[-1].replace("\\n", "")
 wmSize = wmSize.replace("'", "")
-w = int(int(wmSize.split("x")[0]) * scale)
-h = int(int(wmSize.split("x")[1]) * scale)
-wmSize = str(w)+"x"+str(h)
+wmSize = wmSize.replace("\\r", "")
+w = wmSize.split("x")[0]
+h = wmSize.split("x")[1]
 
-# # for device in conectedDevices:
-# #     sendMessage(device, "shell input keyevent 26")
-#     # sendMessage(device, "input keyevent 3")
-#     # sendMessage(device, "input swipe 100 500 100 1450 100")
-#     # sendMessage(device, "wm size", True)
-#     # sendMessage(device, "screencap -p /sdcard/screen.png")
-#     # sendMessage(device, "rm /sdcard/screen.png")
-#     # sendMessage(device, "rm /sdcard/sc.png")
+screenshot()
 
+# for device in conectedDevices:
+#     sendMessage(device, "shell input keyevent 26")
+    # sendMessage(device, "input keyevent 3")
+    # sendMessage(device, "input swipe 100 500 100 1450 100")
+    # sendMessage(device, "wm size", True)
+    # sendMessage(device, "screencap -p /sdcard/screen.png")
+    # sendMessage(device, "rm /sdcard/screen.png")
+    # sendMessage(device, "rm /sdcard/sc.png")
+
+root = Tk()
+root.title("Android Devices")
+root.geometry(wmSize)
+bg = PhotoImage(file = "screen.png")
+# wall_label = Label(image = wall)
+# wall_label.place(x = -2,y = -2)
+
+canvas= Canvas(root, width=w, height=h)
     
 def key(event):
     value = repr(event.char)
     keyValue = str(value).replace("'", "")
     if keyValue == str(1):
         keyValue = 26
-    elif keyValue == "s":
+    if keyValue == "s":
         screenshot()
-        changeImage()
-        return
+        global canvas
+        global bgImg
+        bg = PhotoImage(file = "screen.png")
+        canvas.itemconfig(bgImg, image=bg)
+        canvas.update_idletasks()
     for device in conectedDevices:
         sendMessage(device, "shell input keyevent {}".format(keyValue))
     print("pressed", keyValue)
 
 def callback(event):
     for device in conectedDevices:
-        sendMessage(device, "shell input tap {} {}".format(event.x * int(1/scale), event.y * int(1/scale)))
+        sendMessage(device, "shell input tap {} {}".format(event.x, event.y))
     print("clicked at", event.x, event.y)
 
+# root = Tk()
+# root.title("Android Devices")
+# root.geometry(wmSize)
+# bg = PhotoImage(file = "screen.png")
+# wall_label = Label(image = wall)
+# wall_label.place(x = -2,y = -2)
+
+# canvas= Canvas(root, width=w, height=h)
+canvas.bind("<Key>", key)
+canvas.bind("<Button-1>", callback)
+bgImg = canvas.create_image(0, 0, image=bg, anchor='nw')
+canvas.focus_set()
+canvas.pack()
+
+root.mainloop()
 
 
-def init() :
-    root = Tk()
-    root.title("Android Devices")
-    root.geometry(wmSize)
-
-    canvas= Canvas(root, width=w, height=h)
-    canvas.bind("<Key>", key)
-    canvas.bind("<Button-1>", callback)
-    settingCanvas(canvas)
-
-    screenshot()
-    image = Image.open("screen.jpg")
-    image = image.resize((w, h), Image.ANTIALIAS)
-    bg = ImageTk.PhotoImage(image)
-    bgImgae = canvas.create_image(0, 0, image=bg, anchor='nw')
-
-    canvas.focus_set()
-    canvas.pack()
-    root.mainloop()
-
-def changeImage() :
-    canvas = Canvas(getCanvae())
-    # canvas.delete(bgImgae)
-    # canvas.delete('all')
-    img = Image.open("screen.jpg")
-    img = img.resize((w, h), Image.ANTIALIAS)
-    bgImg = ImageTk.PhotoImage(img)
-    bgImgae = canvas.create_image(0, 0, image=bgImg, anchor='nw')
-    # canvas.itemconfig(bgImgae, image =bgImg)
-    # canvas.update()
-    # canvas.pack()
-    
-
-# screenshot()
-# changeImage()
-
-# image = Image.open("screen.jpg")
-# bg = ImageTk.PhotoImage(image)
-# label = Label(root, image=bg)
-# label.image = bg 
-
-def Image2():
-    canvas.delete("all")
-    # # image1 = PhotoImage(file = "image2.png")
-    # # canvas.create_image(0,0,anchor='nw',image=image1)
-    # canvas.image = image1
-
-
-# stopbutton = Button(root, text='Stop',width=5,fg="red",command = Image2)
-# stopbutton.pack(side = RIGHT)
-
-def settingCanvas(canvas) :
-    can = canvas
-
-def getCanvae() :
-    return can
-
-init()
